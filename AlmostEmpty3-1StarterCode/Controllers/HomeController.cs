00108761f24906681aada671302d3f279d9df518
arcum-omni <hut8dev@gmail.com>
2020-01-07 11:26:51 -0800

    Created basic MVC website with layout

---
+++ AlmostEmpty3-1StarterCode/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlmostEmpty3_1StarterCode.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Precoded index page
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Hut8 Home Page";
+            return View();
+        }
+
+        /// <summary>
+        /// About page for website
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult About()
+        {
+            //ViewData["Title"] = "About Hut8"; // Not the professional way
+            return View();
+        }
+    }
+}
